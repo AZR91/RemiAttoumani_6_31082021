a6f652a346ed5c841ec85aa6e3ddf7ac011c01f2
azr91 <remiattoumani@gmail.com>
2021-09-14 17:54:16 +0200

    creation des fichiers app.js sauce.js et user.js

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require('helmet');
+const path = require("path");
+require('dotenv').config();
+
+const userRoutes = require('./routes/user')
+const sauceRoutes = require('./routes/sauce');
+
+mongoose
+.connect(
+    
+    process.env.SECRET_DB,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log("Connexion à MongoDB réussie !"))
+.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(helmet());
+app.use((req, res, next) => {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+);
+res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+);
+next();
+});
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes)
+
+module.exports = app;
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://AZR91:<password>@cluster0.z8fdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+client.close();
+});
